0d19e2c4115166615520d77918c4fec9098feece
Евгения <jane_79@list.ru>
2022-09-03 15:31:17 +0300

    refactoring

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,59 +1,59 @@
 package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repositories.UsersRepository;
 
-import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+
     @Autowired
-    private UserDao userDao;
+    private UsersRepository usersRepository;
 
     @Override
     public List<User> getAllUsers() {
-        return userDao.getAllUsers();
+        return usersRepository.findAll();
     }
 
     @Override
     public User getUserById(Long id) {
-        try {
-            return userDao.getUserById(id);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        Optional<User> user = usersRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new UsernameNotFoundException("User not found by ID: " + id);
         }
     }
 
     @Transactional
     @Override
     public void saveUser(User user) {
-        try {
-            userDao.saveUser(user);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        usersRepository.save(user);
     }
 
     @Transactional
     @Override
     public User updateUserById(Long id, User updatedUser) {
-        try {
-            User user = userDao.getUserById(id);
+        Optional<User> userContainer = usersRepository.findById(id);
+        if (userContainer.isPresent()) {
+            User user = userContainer.get();
             user.setName(updatedUser.getName());
             user.setSurname(updatedUser.getSurname());
-            return userDao.updateUserById(id, user);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return usersRepository.save(user);
+        } else {
+            throw new UsernameNotFoundException("User not found by ID: " + id);
         }
     }
 
     @Transactional
     @Override
     public void deleteUserById(Long id) {
-        userDao.deleteUserById(id);
+        usersRepository.deleteById(id);
     }
 }
