e24ab982c86a1250c148fc946095c734a2766ab3
Евгения <jane_79@list.ru>
2022-09-05 15:08:20 +0300

    ROLE is fixed

--- src/main/java/ru/kata/spring/boot_security/demo/configs/LoginSuccessHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/LoginSuccessHandler.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,7 +15,9 @@ import java.util.Set;
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         log.info("roles: {}", roles);
         if (roles.contains("ROLE_ADMIN")) {
